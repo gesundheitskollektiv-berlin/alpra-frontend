4397a9f4d963edae48ff2d36e495695d219005f1
Andreas Neumann <andneuma@posteo.de>
2026-05-10 15:53:10 +0200

    Restyle service section

--- src/lib/components/page-blocks/ServicesBlock.svelte
+++ src/lib/components/page-blocks/ServicesBlock.svelte
@@ -19,7 +19,7 @@
 				<h2>{data.title}</h2>
 
 				{#if data?.content}
-					<div class="mt-4">
+					<div class="mt-4 services-content">
 						{@html resolveRichText(data.content)}
 					</div>
 				{/if}
@@ -33,7 +33,42 @@
 		line-height: 2;
 	}
 
+	.services-content {
+		--arrow-indent: calc(0.625rem + 0.6rem);
+	}
+
+	.services-content :global(> *:not(h4)) {
+		padding-left: var(--arrow-indent);
+	}
+
 	section :global(h4) {
 		margin-top: 2rem;
+		display: flex;
+		align-items: baseline;
+		gap: 0.6rem;
+	}
+
+	section :global(h4)::before {
+		content: '';
+		flex: 0 0 auto;
+		display: inline-block;
+		width: 0.625rem;
+		height: 0.875rem;
+		background-image: url('/images/Pfeil-Liste.svg');
+		background-repeat: no-repeat;
+		background-size: contain;
+		background-position: center;
+		transform: translateY(0.05em);
+	}
+
+	section :global(ul) {
+		list-style: none;
+		list-style-image: none;
+		margin-left: 0;
+		padding-left: 0;
+	}
+
+	section :global(li) {
+		margin-top: 0.15rem;
 	}
 </style>
